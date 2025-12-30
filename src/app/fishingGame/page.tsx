0c3866357b927a0ca86ace5fb6bc6f948cf7f201
정현태 <jht3021@gmail.com>
2025-12-30 10:25:36 +0900

    feat : 낚시터 상세 설명 화면

--- src/app/fishingGame/page.tsx
+++ src/app/fishingGame/page.tsx
@@ -1,23 +1,25 @@
 "use client"
 
 import * as _ from "./style";
+import { useRouter } from "next/navigation";
 
 
 const ChoiceMap = () => {
+    const router = useRouter();
     return (
         <_.Container>
             <_.Background src="/assets/map.png" />
-            <_.Mark>
+            <_.Mark onClick={() => router.push("/fishingGame/west")}>
                 서해
                 <_.MarkImage src="/assets/mark.svg" />
 
             </_.Mark>
-            <_.Mark2>
+            <_.Mark2 onClick={() => router.push("/fishingGame/south")}>
                 남해
                 <_.MarkImage src="/assets/mark.svg" />
 
             </_.Mark2>
-            <_.Mark3>
+            <_.Mark3 onClick={() => router.push("/fishingGame/east")}>
                 동해
                 <_.MarkImage src="/assets/mark.svg" />
 
